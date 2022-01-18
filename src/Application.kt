e5495785fb858d63e8643f27667b2746f2a3cb7b
n3gbx <17089577+n3gbx@users.noreply.github.com>
2022-01-18 20:15:14 +0100

    Update db data path

--- src/Application.kt
+++ src/Application.kt
@@ -180,7 +180,7 @@ private fun Application.setupAppConfig() {
     System.setProperty("handlers", "org.slf4j.bridge.SLF4JBridgeHandler")
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
 
-    val dbDataPath = System.getProperty("DB_DATA_PATH", "resources/db_data")
+    val dbDataPath = System.getProperty("DB_DATA_PATH", "resources/db/data")
     val hoconConfig = this.environment.config.config("ktor")
 
     appConfig.apply {
