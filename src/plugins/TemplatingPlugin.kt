0d4330f4c45d7dc76186694099107eba51ef8373
n3gbx <17089577+n3gbx@users.noreply.github.com>
2022-07-16 23:35:14 +0200

    Refactor

--- src/plugins/TemplatingPlugin.kt
+++ src/plugins/TemplatingPlugin.kt
@@ -1,11 +1,13 @@
 package dev.ducket.api.plugins
 
 import freemarker.cache.ClassTemplateLoader
+import freemarker.core.HTMLOutputFormat
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 
 fun Application.installTemplatingPlugin() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
     }
 }
